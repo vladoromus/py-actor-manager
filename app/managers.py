c207952118977953403885d2874163ca62cbaf6f
Vladyslav <vladoromus@gmail.com>
2025-05-27 19:41:01 +0200

    solution2

--- app/managers.py
+++ app/managers.py
@@ -26,7 +26,7 @@ class ActorManager:
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.conn.execute(
-            "UPDATE actors SET first_name = ?, last_name = ? WHERE id = ?",
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?",
             (new_first_name, new_last_name, pk)
         )
 
